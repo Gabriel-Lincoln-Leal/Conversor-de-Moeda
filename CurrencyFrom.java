084aee41932d60bfc75322ff4044817e907591ba
Gabriel-Lincoln-Leal <gabriellincolnleal01@gmail.com>
2025-11-13 15:46:21 -0300

    Add files via upload

--- CurrencyFrom.java
+++ CurrencyFrom.java
@@ -1,17 +1,19 @@
+package Model;
+
 import java.util.Map;
 
 public class CurrencyFrom {
-    private String result;
+
+    /* private String result;
     private String documentation;
     private String terms_of_use;
     private long time_last_update_unix;
     private String time_last_update_utc;
     private long time_next_update_unix;
-    private String time_next_update_utc;
+    private String time_next_update_utc; */
+
     private String base_code;
     private Map<String, Double> conversion_rates;
-
-
 
     public String getBaseCode() {
         return base_code;
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+package Main;
+
+import Model.CurrencyFrom;
 import com.google.gson.Gson;
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -9,45 +11,94 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String moedaDe, moedaPara;
+    public static void main(String[] args) {
+
+        String moedaDe;
+        String moedaPara;
         double valor;
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Converter moedaDe:");
-        moedaDe = sc.nextLine();
-        System.out.println("Converter para:");
-        moedaPara = sc.nextLine();
-        System.out.println("Quantidade:");
-        valor = sc.nextDouble();
-        System.out.println(moedaDe);
-        System.out.println(moedaPara);
-        sc.close();
 
-        String url = "https://v6.exchangerate-api.com/v6/4a7036080647de485e397b77/latest/" + moedaDe;
+        try {
+            while (true) {
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
+                System.out.println("""
+               \s
+                1. Dolar (USD) para Reais (BRL).
+                2. Reais (BRL) para Dolar (USD).
+                3. Euros (EUR) para Reais (BRL)
+                4. Reais (BRL) para Euros (EUR).
+                5. Rublos (RUB) para Reais (BRL).
+                6. Reais (BRL) para Rublos (RUB).
+               \s
+                7. Sair.
+               \s
+                Escolha uma opção:""");
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+                int opcao = sc.nextInt();
 
-        Gson gson = new Gson();
+                switch (opcao) {
+                    case 1:
+                        moedaDe = "USD";
+                        moedaPara = "BRL";
+                        break;
+                    case 2:
+                        moedaDe = "BRL";
+                        moedaPara = "USD";
+                        break;
+                    case 3:
+                        moedaDe = "EUR";
+                        moedaPara = "BRL";
+                        break;
+                    case 4:
+                        moedaDe = "BRL";
+                        moedaPara = "EUR";
+                        break;
+                    case 5:
+                        moedaDe = "RUB";
+                        moedaPara = "BRL";
+                        break;
+                    case 6:
+                        moedaDe = "BRL";
+                        moedaPara = "RUB";
+                        break;
+                    case 7:
+                        moedaDe = null;
+                        moedaPara = null;
+                        break;
+                    default:
+                        System.out.println("Digite apenas números de 1 à 7.\n");
+                        continue;
+                }
+                if (moedaDe == null) {
+                    break;
+                }
+                System.out.println("Informe o valor para converter:");
+                valor = sc.nextDouble();
 
-        CurrencyFrom moeda = gson.fromJson(response.body(), CurrencyFrom.class);
+                String url = "https://v6.exchangerate-api.com/v6/4a7036080647de485e397b77/latest/" + moedaDe;
 
-        Map<String, Double> rates = moeda.getConversionRates();
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(url))
+                        .build();
 
-        Double taxa = rates.get(moedaPara);
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(valor * taxa);
+                Gson gson = new Gson();
 
+                CurrencyFrom moeda = gson.fromJson(response.body(), CurrencyFrom.class);
+
+                Map<String, Double> rates = moeda.getConversionRates();
+
+                Double taxa = rates.get(moedaPara);
+
+                System.out.println("Valor convertido de " + moedaDe + " para " + moedaPara + " é " + valor * taxa);
+
+            }
+        } catch (Exception e) {
+            System.out.println("Digite apenas números.");
         }
     }
-
-
-
-
-
+}
